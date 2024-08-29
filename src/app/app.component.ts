58f5324a3915c0eaa8bfc618d189c34c82be36fe
annderoli <annderoli@gmail.com>
2024-08-29 14:55:00 -0300

    construção service order

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    MenuComponent, 
+    HeaderComponent, 
+    SummaryComponent,
+    DashboardComponent
+
+  ],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
